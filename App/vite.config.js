b1066bcae6a711088f155302df6e55e096d99a2d
MAT�AS JOAQUIN CASTERA <48385887@ort>
2025-05-28 08:20:11 -0300

    ak

--- App/vite.config.js
+++ App/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/TP5_PedidoDeEmpanadas/',
+  "homepage": "https://github.com/tiagoghel/TP5_PedidoDeEmpanadas.git"
+
 })
